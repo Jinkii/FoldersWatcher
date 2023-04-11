8a37a3c73bd21feb1dcca2c71b876fe8fd5b0311
Jinkii <jinki921@gmail.com>
2023-04-11 16:48:55 +0200

    Fixed dependencies

--- FoldersWatcher/FoldersWatcher.cs
+++ FoldersWatcher/FoldersWatcher.cs
@@ -1,11 +1,10 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
-using System.IO;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Configuration;
-using NLog;
 
 namespace FoldersWatcher
 {
